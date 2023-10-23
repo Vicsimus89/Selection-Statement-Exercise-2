80ca3855e7ecc0b3b13df784e4979cbe177b9f98
Vicsimus89 <fastpitching06801@msn.com>
2023-10-23 17:37:42 -0600

    finished selection statement exercise 2, I mad a switch statement regarding five school subjects, and added a default case for ones I couldn't list.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your favorite school subject?");
+            string userInput = Console.ReadLine();
+            string subject = userInput;
+            switch (userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Great, math is a fun but hard subject.");
+                    break;
+                case "english":
+                    Console.WriteLine("Wow, English I never really liked but that's great you do.");
+                    break;
+                case "art":
+                    Console.WriteLine("That's awesome, that's my favorite subject as well!");
+                    break;
+                case "pe":
+                    Console.WriteLine("Good for you, I didn't start liking it until I joined the military.");
+                    break;
+                case "science":
+                    Console.WriteLine("That's a fun and hard subject, I like it as well.");
+                    break;
+                default:
+                    Console.WriteLine("That's great! Unfortunately there were a lot of great subjects and I couldn't list them all.");
+                    break;
+            }
         }
     }
 }
